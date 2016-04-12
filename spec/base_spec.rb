477fe1c43d5e1292e7491187fdfcc6adec44573e
Grant Birchmeier <gbirchmeier@connamara.com>
2016-04-12 11:37:53 -0500

    specs written while investigating #20

---
+++ spec/base_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe ActiveAdminDatetimepicker::Base do
+
+  class Dummy
+    include ActiveAdminDatetimepicker::Base
+  end
+
+  let(:dummy) { Dummy.new }
+
+  it 'html_class' do
+    expect(dummy.html_class).to eq "date-time-picker"
+  end
+
+  it 'format' do
+    expect(dummy.format).to eq '%Y-%m-%d %H:%M'
+    expect(ActiveAdminDatetimepicker::Base.format).to eq '%Y-%m-%d %H:%M'
+  end
+
+end
