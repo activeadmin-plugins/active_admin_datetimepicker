edca8631f313f8e855395a6fd356e81e334c25f4
Denys Metelov <alatelb@gmail.com>
2022-04-07 17:43:58 +0300

    Remove coffee script dependency

---
+++ app/assets/javascripts/active_admin_datetimepicker.js
@@ -0,0 +1,25 @@
+//= require jquery.xdan.datetimepicker.full
+
+function setupDateTimePicker(container) {
+  const defaults = {
+    formatDate: 'y-m-d',
+    format: 'Y-m-d H:i',
+    allowBlank: true,
+    defaultSelect: false,
+    validateOnBlur: false
+  }
+
+  $(container).find('input.date-time-picker').each(function(index, entry) {
+    $(entry).datetimepicker($.extend(defaults, $(entry).data('datepicker-options')));
+  });
+}
+
+function initXdanDateTimePickerPlugin() {
+  setupDateTimePicker($('body'));
+  $(document).on('has_many_add:after', '.has_many_container', function(e, fieldset) {
+    setupDateTimePicker(fieldset);
+  });
+}
+
+$(document).ready(initXdanDateTimePickerPlugin);
+$(document).on('turbolinks:load', initXdanDateTimePickerPlugin());
